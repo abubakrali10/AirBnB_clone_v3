f9e75b451e9f9d7ba079f4c2f11487415bb84d99
abubakrali10 <bakrali175@yahoo.com>
2023-10-01 03:41:04 +0300

    fix output

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,11 @@
 """
 module to create routes for API
 """
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def api_status():
     """returns api status"""
     status = {"status": "OK"}
