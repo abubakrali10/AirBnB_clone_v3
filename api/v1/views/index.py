779e3c4186eca9aca2770c8488ea795ff5ad1ab1
abubakrali10 <bakrali175@yahoo.com>
2023-10-01 21:17:47 +0300

    create endpoint for stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,25 @@ module to create routes for API
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+classes = {"amenities": "Amenity", "cities": "City",
+           "places": "Place", "reviews": "Review",
+           "states": "State", "users": "User"}
+
+
+@app_views.route('/status')
 def api_status():
     """returns api status"""
     status = {"status": "OK"}
     return jsonify(status)
+
+
+@app_views.route('stats')
+def app_stats():
+    """retrieving statistics of different objects"""
+    stats = {}
+    for k, v in classes.items():
+        stats[k] = storage.count(v)
+    return jsonify(stats)
