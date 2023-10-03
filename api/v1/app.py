3b013fb3de9190730b79190671f2288009c72677
abubakrali10 <bakrali175@yahoo.com>
2023-10-03 04:47:33 +0300

    update for virtual environment

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,6 +29,6 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST")
-    port = int(os.getenv("HBNB_API_PORT"))
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
