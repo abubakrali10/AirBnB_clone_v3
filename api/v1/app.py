8f5997f6a0ab18688bd96be61ed4e11addaae7fd
abubakrali10 <bakrali175@yahoo.com>
2023-10-03 04:41:17 +0300

    add CORS to the app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,13 @@
 """The flask app"""
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
